5cb53e961f208d52c9fa6a3ba63dc1d78c600a97
Sergey Zhilin <s.zhilin@semrush.com>
2018-09-01 13:27:00 +0300

    folders cvhanged

---
+++ js/README.md
@@ -0,0 +1,14 @@
+# Js part of quiz
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm i
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ js/src/components/TextContent.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="section with-image">
+    <div class="container">
+      <div class="tile is-ancestor justified-tile">
+        <div class="tile is-6">
+          <article class="tile is-child">
+
+            <div class="tile is-4">
+              <p class="title">Привет)</p>
+            </div>
+            <p class="title" style="text-align: center; color: white">Маленькая шалость</p>
+            <div class="content">
+              <p class="text-content">
+                В конце каждого дня тут будут появляться небольшие задания
+                просто для того, чтобы немного отдохнуть от рабочей рутины.
+              </p>
+              <p class="text-content">
+                Но вот только просто так выполнять задания - это скучно.
+              </p>
+              <p class="text-content">
+                Поэтому у тебя есть возможность подзаработать немного и потом
+                потратить эти кредиты (поцелуйчики) с умом!
+              </p>
+              <p class="text-content">
+                По любым вопросам ты знаешь куда обращаться.
+                Если закончилась зарядка для мозгов - ты опять знаешь куда идти ;)
+              </p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style>
+  p.text-content {
+    text-align: justify;
+    color: white;
+  }
+
+  .justified-tile {
+    justify-content: center;
+  }
+
+  .tile {
+    display: inline;
+  }
+
+  .with-image {
+    background: url("../assets/tada.png") no-repeat center center fixed;
+  }
+</style>

98922acfce8f4387a17f3ee9cfdd5359a776b99e
Sergey Zhilin <s.zhilin@semrush.com>
2018-09-08 04:12:00 +0300

    mongo config fixed

--- java-quiz/src/main/java/me/quiz/config/MongoConfig.java
+++ java-quiz/src/main/java/me/quiz/config/MongoConfig.java
@@ -3,6 +3,8 @@ package me.quiz.config;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -13,15 +15,22 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ComponentScan("me.quiz.mongo.repo")
 public class MongoConfig extends AbstractMongoConfiguration {
 
+    @Value("${mongo.host:quiz-mongodb}")
+    public String host;
+
+    @Value("${mongo.db:quest}")
+    public String db;
+
     @Override
     protected String getDatabaseName() {
-        return "quest";
+        return db;
     }
 
+    @Bean
     @Override
     public Mongo mongo() throws Exception {
         return new MongoClient(
-                new ServerAddress("localhost", 27017)
+                new ServerAddress(host, 27017)
         );
     }
 }

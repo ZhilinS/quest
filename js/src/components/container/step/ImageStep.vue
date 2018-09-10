138890903e0bf77cd23baa7145f19233f53cbc3f
Sergey Zhilin <s.zhilin@semrush.com>
2018-09-10 22:51:08 +0300

    All image added

--- js/src/components/container/step/ImageStep.vue
+++ js/src/components/container/step/ImageStep.vue
@@ -1,12 +1,32 @@
 <template>
     <section class="section with-image">
-        <div class="container">
-            <img src="../../../assets/memes.jpg" alt="memes">
+        <div class="container pointed-container">
+            <img src="../../../assets/got_simple.jpg" alt="got_simple" v-if="!clicked" @click="toggle_img">
+            <img src="../../../assets/got.jpg" alt="got" v-if="clicked" @click="toggle_img">
         </div>
     </section>
 </template>
 
 <script>
+
+    export default {
+      data() {
+        return {
+          clicked_img: false
+        }
+      },
+      computed: {
+        clicked() {
+          return this.clicked_img;
+        }
+      },
+
+      methods: {
+        toggle_img() {
+          this.clicked_img = !this.clicked_img
+        }
+      }
+    }
 
 </script>
 
@@ -15,5 +35,9 @@
         background: url("../../../assets/tada.png") no-repeat center center fixed;
         display: flex;
         justify-content: center;
+    }
+
+    .pointed-container {
+        cursor: pointer;
     }
 </style>
